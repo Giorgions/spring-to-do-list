679cc94d42414b31343d5ea87130127f6ba9c88a
grlukasa <88y57U9011>
2022-04-24 09:54:30 +0200

    Initial commit

---
+++ src/main/java/angularspring/springtodolist/SpringToDoListApplication.java
@@ -0,0 +1,13 @@
+package angularspring.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToDoListApplication.class, args);
+	}
+
+}
